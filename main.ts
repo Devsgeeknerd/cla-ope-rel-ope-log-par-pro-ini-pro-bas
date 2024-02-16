a6cc36ee7069a5a7700f4bd8985a4f344f2f8efc
Paulo Vanderley <vanderley.1109@gmail.com>
2024-02-16 15:01:37 -0300

    adicionando conteudo

---
+++ main.ts
@@ -0,0 +1,41 @@
+// Comparação de números.
+// let num1: number = 10;
+// let num2: number = 20;
+
+// Comparação de maior que.
+// let maiorQue: boolean = num1 > num2;
+// console.log(maiorQue);
+
+// Comparação de menor que.
+// let menorQue: boolean = num1 < num2;
+// console.log(menorQue);
+
+// Comparação de maior ou igual que.
+// let maiorOuIgualQue: boolean = num1 >= num2;
+// console.log(maiorOuIgualQue);
+
+// Comparação de menor ou igual que.
+// let menorOuIgualQue: boolean = num1 <= num2;
+// console.log(menorOuIgualQue);
+
+// Comparação de igualdade.
+// let igualQue: boolean = num1 === num2;
+// console.log(igualQue);
+
+// Comparação de diferença.
+// let diferenteQue: boolean = num1 !== num2;
+// console.log(diferenteQue);
+
+// Comparação de strings.
+let nome1: string = "Paulo";
+let nome2: string = "Vanderley";
+
+// Comparação de igualdade de strings.
+let igualQueStrings: boolean = nome1 === nome2;
+console.log(igualQueStrings);
+
+// Comparação de diferença de strings.
+let diferenteQueStrings: boolean = nome1 !== nome2;
+console.log(diferenteQueStrings);
+
+
